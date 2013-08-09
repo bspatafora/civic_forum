09db913f0d3aa6f10bd582939de159f19ca40455
bspatafora <benjamin.spatafora@gmail.com>
2013-08-09 00:20:35 -0500

    Added login requirement to views and set up basic login view and template

--- civically/urls.py
+++ civically/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,4 +11,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
--- postings/urls.py
+++ postings/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from postings import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.Feed.as_view(),
+    url(r'^$', login_required(views.Feed.as_view()),
         name='feed',),
-    url(r'^create_posting$', views.CreatePosting.as_view(),
+    url(r'^create_posting$', login_required(views.CreatePosting.as_view()),
         name='create_posting',),
-    url(r'^(?P<pk>\d+)$', views.Detail.as_view(),
+    url(r'^(?P<pk>\d+)$', login_required(views.Detail.as_view()),
         name='detail',),
-    url(r'^delete_posting/(?P<pk>\d+)$', views.DeletePosting.as_view(),
+    url(r'^delete_posting/(?P<pk>\d+)$', login_required(views.DeletePosting.as_view()),
         name='delete_posting',),
-    url(r'^create_comment$', views.CreateComment.as_view(),
+    url(r'^create_comment$', login_required(views.CreateComment.as_view()),
         name='create_comment',),
-    url(r'^delete_comment/(?P<pk>\d+)$', views.DeleteComment.as_view(),
+    url(r'^delete_comment/(?P<pk>\d+)$', login_required(views.DeleteComment.as_view()),
         name='delete_comment',),
 )
