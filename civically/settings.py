c4a4114e18ee6745ae9581d8b760893a8c3860be
bspatafora <benjamin.spatafora@gmail.com>
2013-12-04 20:07:00 -0600

    Worked on email digest

--- civically/settings.py
+++ civically/settings.py
@@ -169,3 +169,9 @@ AUTHENTICATION_BACKENDS = (
 GUARDIAN_RENDER_403 = True
 
 ANONYMOUS_USER_ID = -1 # Required by django-guardian
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'civically.digest@gmail.com'
+EMAIL_HOST_PASSWORD = ***REMOVED***
